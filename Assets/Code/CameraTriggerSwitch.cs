2ffa38907ab10fd57bcd98e45523542a51344862
WesInAPlace <143664522+WIAPlace@users.noreply.github.com>
2026-01-26 22:31:25 -0600

    Playable Put Together
    Added stuff to work together, u were all there, u should know

--- Assets/Code/CameraTriggerSwitch.cs
+++ Assets/Code/CameraTriggerSwitch.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 // Camera trigger switch
 // Created: 1/25 Weston T
 // Purpose: when player enters the trigger switch the camera to this one.
+// Edit: Add Enables for Ghosts
 
 public class CameraTriggerSwitch : MonoBehaviour
 {
@@ -17,20 +18,37 @@ public class CameraTriggerSwitch : MonoBehaviour
     [Tooltip("whatever the player's layer is.")]
     private LayerMask playerLayer;
 
+    [SerializeField]
+    [Tooltip("Holds the Ghosts in the Room")]
+    private GameObject[] Ghosts;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //Debug.Log("Entered;");
         if((playerLayer.value & (1 << other.gameObject.layer)) != 0)
         { // Only trigger if the player moves into this room
+            //Debug.Log("Entered;" + roomCam);
             roomCam.Priority = 15; // Higher priority so this will be the used camera.
+            foreach(GameObject roomGhost in Ghosts)
+            { // when you enter the room set the ghosts to active
+                if(roomGhost != null){ // if it still exists
+                    roomGhost.SetActive(true);
+                }
+            }
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        //Debug.Log("Exited;");
+        
         if((playerLayer.value & (1 << other.gameObject.layer)) != 0)
         { // Only trigger if the player moves into this room
+            //Debug.Log("Exited;"+ roomCam);
             roomCam.Priority = 5; // Lower the priority so the next room will be highest priority
+            foreach(GameObject roomGhost in Ghosts)
+            { // when you enter the room set the ghosts to false
+                if(roomGhost != null){ // if it still exists
+                    roomGhost.SetActive(false);
+                }
+            }
         }
     }
 }
--- Assets/HealthBar/Hearts.cs
+++ Assets/HealthBar/Hearts.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Created by: Sam Saeger
+// edited 1/26 by weston tollet - added connection between is and the players health script.
 public class Hearts : MonoBehaviour
 {
-    public int health;
-    public int maxHealth;
+    // current health will always be the player's heath.
+    public int health => playerHealth.GetHealth();
+    public int maxHealth = 3;
 
     public Sprite emptyHeart;
     public Sprite fullHeart;
     public Image[] hearts;
+
+    [SerializeField]
+    [Tooltip("Player's Health thing")]
+    private Health playerHealth; 
+
 
 
     // Start is called before the first frame update
@@ -22,12 +30,13 @@ public class Hearts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         for (int i = 0; i < hearts.Length; i++)
         {
             if (i < health) 
             {
                 hearts[i].sprite = fullHeart;
-            
+                //Debug.Log(i);
             }
             else
             {
@@ -44,5 +53,6 @@ public class Hearts : MonoBehaviour
                 hearts[i].enabled = false;
             }
         }
+        
     }
 }
